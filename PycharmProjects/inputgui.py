5768e7153e9a020224fea13e176448362933ba6f
Akshat <akshatjain24may@gmail.com>
2020-10-02 14:43:30 +0530

    input gui

---
+++ PycharmProjects/inputgui.py
@@ -0,0 +1,86 @@
+from tkinter import *
+
+root = Tk()
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.title("runway allocation")
+root.minsize(width=400, height=400)
+root.geometry("%dx%d+0+0" % (w, h))
+root.configure(background="#121212")
+data=[]
+options={"international","diplomatic","private","domestic","cargo"}
+variable = StringVar(root)
+variable.set("choose")
+def addinfo():
+    typeOfFlight=variable.get()
+    nameOfFlight=nameOfFlightEntry.get()
+    flightNo=flightNoEntry.get()
+    hr=int(hrEntry.get())
+    min=int(minEntry.get())
+    startBufferTime=int(startBufferTimeEntry.get())
+    endBufferTime=int(endBufferTimeEntry.get())
+    a=[typeOfFlight,nameOfFlight,flightNo,hr,min,startBufferTime,endBufferTime]
+    data.append(a)
+    getinfo()
+
+def addinfoanddestroy():
+    typeOfFlight = variable.get()
+    nameOfFlight = nameOfFlightEntry.get()
+    flightNo = flightNoEntry.get()
+    hr = int(hrEntry.get())
+    min = int(minEntry.get())
+    startBufferTime = int(startBufferTimeEntry.get())
+    endBufferTime = int(endBufferTimeEntry.get())
+    a = [typeOfFlight, nameOfFlight, flightNo, hr, min, startBufferTime, endBufferTime]
+    data.append(a)
+    root.destroy()
+
+def getinfo():
+    global root, typeOfFlightEntry, nameOfFlightEntry, flightNoEntry, hrEntry, minEntry, startBufferTimeEntry, endBufferTimeEntry, data
+
+    lb1 = Label(text="Type of flight : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb1.place(relx=0.3,rely=0.095, relwidth=0.15, relheight=0.05)
+
+    typeOfFlightEntry = OptionMenu(root,variable, *options)
+    #typeOfFlightEntry = Entry(bg='#C4C4C4')
+    typeOfFlightEntry.place(relx=0.5,rely=0.1, relwidth=0.3, relheight=0.05)
+
+    lb2 = Label(text="Name of Airline : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb2.place(relx=0.3,rely=0.195, relwidth=0.15, relheight=0.05)
+    nameOfFlightEntry = Entry(bg='#C4C4C4')
+    nameOfFlightEntry.place(relx=0.5,rely=0.2, relwidth=0.3, relheight=0.05)
+
+    lb3 = Label(text="Flight No : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb3.place(relx=0.3,rely=0.295, relwidth=0.15, relheight=0.05)
+    flightNoEntry = Entry(bg='#C4C4C4')
+    flightNoEntry.place(relx=0.5,rely=0.3, relwidth=0.3, relheight=0.05)
+
+    lb4 = Label(text="Landing Time : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb4.place(relx=0.3,rely=0.395, relwidth=0.15, relheight=0.05)
+    hrEntry=Spinbox(root,from_=0 , to =24,bg='#C4C4C4')
+    hrEntry.place(relx=0.5,rely=0.4,relwidth=0.07,relheight=0.05)
+
+    lb7 = Label(text="hrs",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb7.place(relx=0.58,rely=0.395, relwidth=0.02, relheight=0.05)
+    minEntry=Spinbox(root,from_=0 , to =59,bg='#C4C4C4')
+    minEntry.place(relx=0.63,rely=0.4,relwidth=0.07,relheight=0.05)
+    lb8 = Label(text="mins",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb8.place(relx=0.71,rely=0.395, relwidth=0.035, relheight=0.05)
+
+    lb5 = Label(text="Start Buffer Time : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb5.place(relx=0.3,rely=0.495, relwidth=0.15, relheight=0.05)
+    startBufferTimeEntry = Entry(bg='#C4C4C4')
+    startBufferTimeEntry.place(relx=0.5,rely=0.5, relwidth=0.3, relheight=0.05)
+
+    lb6 = Label(text="End Buffer Time : ",bg="#121212",fg='white',font=("Times new Roman", 24))
+    lb6.place(relx=0.3,rely=0.595, relwidth=0.15, relheight=0.05)
+    endBufferTimeEntry = Entry(bg='#C4C4C4')
+    endBufferTimeEntry.place(relx=0.5,rely=0.6, relwidth=0.3, relheight=0.05)
+
+    SubmitBtn = Button(root,text="SUBMIT",bg='#03DAC6',activebackground='#03DAC6', fg='black',font=("Times new Roman", 24),command=addinfoanddestroy)
+    SubmitBtn.place(relx=0.35,rely=0.72, relwidth=0.18,relheight=0.08)
+    NxtBtn = Button(root,text="Next",bg='#03DAC6', fg='black',activebackground='#03DAC6',font=("Times new Roman", 24),command=addinfo)
+    NxtBtn.place(relx=0.57,rely=0.72, relwidth=0.18,relheight=0.08)
+    root.mainloop()
+getinfo()
+
+print(data)
