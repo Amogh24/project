f2bb445159ecce470e6d0a5860f3c1574bc04b5a
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2020-10-03 17:32:10 +0530

    removed priority

--- PycharmProjects/priority2.py
+++ PycharmProjects/priority2.py
@@ -1,28 +1,27 @@
 from inputgui import data
 import heap2
 import random
-priority = []
+
 for a in data:
     if a[0] == 1:
-        a[0] = 7500
-        priority.append(a)
+        a[0] = 7500 + random.randint(1, 1000)
+
     elif a[0] == 2:
-        a[0] = 6000
-        priority.append(a)
+        a[0] = 6000 + random.randint(1, 1000)
+
     elif a[0] == 3:
-        a[0] = 4500
-        priority.append(a)
+        a[0] = 4500 + random.randint(1, 1000)
+
     elif a[0] == 4:
-        a[0] = 3000
-        priority.append(a)
+        a[0] = 3000 + random.randint(1, 1000)
+
     elif a[0] == 5:
-        a[0] = 1500
-        priority.append(a)
-for a in priority:
-    a[0] = a[0]+random.randint(1, 1000)
+        a[0] = 1500 + random.randint(1, 1000)
 
-heap2.heapify(priority)
 
-for b in priority:
+
+heap2.heapify(data)
+
+for b in data:
     print(b)
 
