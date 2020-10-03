ad3a6a878acb70112c876b00f1e06d9eb5b34d75
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2020-10-03 17:23:27 +0530

    entire node

--- PycharmProjects/priority2.py
+++ PycharmProjects/priority2.py
@@ -1,27 +1,27 @@
-from input import data
-import heap
+from inputgui import data
+import heap2
 import random
 priority = []
 for a in data:
     if a[0] == 1:
-        b = [7500, a[2], a[3]]
-        priority.append(b)
+        a[0] = 7500
+        priority.append(a)
     elif a[0] == 2:
-        b = [6000, a[2], a[3]]
-        priority.append(b)
+        a[0] = 6000
+        priority.append(a)
     elif a[0] == 3:
-        b = [4500, a[2], a[3]]
-        priority.append(b)
+        a[0] = 4500
+        priority.append(a)
     elif a[0] == 4:
-        b = [3000, a[2], a[3]]
-        priority.append(b)
+        a[0] = 3000
+        priority.append(a)
     elif a[0] == 5:
-        b = [1500, a[2], a[3]]
-        priority.append(b)
-for b in priority:
-    b[0] = b[0]+random.randint(1, 1000)
+        a[0] = 1500
+        priority.append(a)
+for a in priority:
+    a[0] = a[0]+random.randint(1, 1000)
 
-heap.heapify(priority)
+heap2.heapify(priority)
 
 for b in priority:
     print(b)
